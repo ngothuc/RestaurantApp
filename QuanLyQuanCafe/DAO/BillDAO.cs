67329f9b8ab610b077be5fc0056c1b4edffcda3e
Ngo Van Thuc <ngothuc0704@gmail.com>
2023-06-04 20:54:46 +0700

    LastFiles

--- QuanLyQuanCafe/DAO/BillDAO.cs
+++ QuanLyQuanCafe/DAO/BillDAO.cs
@@ -18,6 +18,8 @@ namespace QuanLyQuanCafe.DAO
             private set { BillDAO.instance = value; }
         }
 
+
+
         private BillDAO() { }
         /// <summary>
         /// Thành công: bill ID
